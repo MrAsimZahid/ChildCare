06d4dfe519de73e76c3460754aade546530970a8
Asim Zahid <asimzahid02@gmail.com>
2020-03-24 13:08:44 +0500

    project structure a little bit

---
+++ app/src/main/java/io/librehealth/childcare/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package io.librehealth.childcare.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.librehealth.childcare.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
