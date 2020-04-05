0b57dceb63adaacbe44ab951ec73fe9e6c9f8988
Asim Zahid <asimzahid02@gmail.com>
2020-04-05 21:39:18 +0500

    SignIn screen

---
+++ app/src/main/java/io/librehealth/childcare/ui/FreshSignIn.java
@@ -0,0 +1,16 @@
+package io.librehealth.childcare.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.librehealth.childcare.R;
+
+public class FreshSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fresh_sign_in);
+    }
+}
