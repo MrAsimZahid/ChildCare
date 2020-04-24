7874afb05492afb5c2db47bae70ef57366082890
Asim Zahid <asimzahid02@gmail.com>
2020-04-24 12:55:23 +0500

    👌Settings page completed.

--- app/src/main/java/io/librehealth/childcare/ui/MainActivity.java
+++ app/src/main/java/io/librehealth/childcare/ui/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_settings);
         //setContentView(R.layout.activity_security_option);
 //
 //        try{
